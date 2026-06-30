02f0615d152bb60685c4c888e94b5a3cf89bcff2
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-06-30 15:31:21 +0200

    fix(ex04): fix formatting and comment out main

--- ex04/ft_fibonacci.c
+++ ex04/ft_fibonacci.c
@@ -6,11 +6,11 @@
 /*   By: nfurst <nfurst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/30 15:10:21 by nfurst            #+#    #+#             */
-/*   Updated: 2026/06/30 15:27:03 by nfurst           ###   ########.fr       */
+/*   Updated: 2026/06/30 15:30:36 by nfurst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_fibonacci(int index)
+int	ft_fibonacci(int index)
 {
 	if (index < 0)
 		return (-1);
@@ -21,14 +21,14 @@ int ft_fibonacci(int index)
 	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
 }
 
-#include <stdlib.h>
-#include <stdio.h>
-int main(int argc, char* argv[])
-{
-	int res;
-	if (argc != 2)
-		return (1);
-	res = ft_fibonacci(atoi(argv[1]));
-	printf("Result: %d", res);
-	return (0);
-}
+// #include <stdlib.h>
+// #include <stdio.h>
+// int main(int argc, char* argv[])
+// {
+// 	int res;
+// 	if (argc != 2)
+// 		return (1);
+// 	res = ft_fibonacci(atoi(argv[1]));
+// 	printf("Result: %d", res);
+// 	return (0);
+// }

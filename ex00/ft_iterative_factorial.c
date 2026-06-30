09aa2c4b54ef7894638e216f6b67cf7057e5046a
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-06-30 10:57:22 +0200

    feat(ex00): add function for iterative factorial

---
+++ ex00/ft_iterative_factorial.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfurst <nfurst@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/30 10:33:39 by nfurst            #+#    #+#             */
+/*   Updated: 2026/06/30 10:48:43 by nfurst           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_factorial(int nb)
+{
+	int i;
+	int res;
+
+	if (nb < 0)
+		return (0);
+	i = 1;
+	res = 1;
+	while (i <= nb)
+	{
+		res *= i;
+		i++;
+	}
+	return (res);
+}
+
+/*
+#include <stdio.h>
+
+int main(void)
+{
+	int res;
+
+	res = ft_iterative_factorial(5);
+	printf("%d", res);
+	return (0);
+}
+*/

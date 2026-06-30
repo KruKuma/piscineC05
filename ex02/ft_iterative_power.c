1b5d2e6585f24ca235d5f78f09e7f6e5bca62e32
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-06-30 14:55:14 +0200

    fix(ex02): fix formatting for norminette

--- ex02/ft_iterative_power.c
+++ ex02/ft_iterative_power.c
@@ -6,14 +6,14 @@
 /*   By: nfurst <nfurst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/30 11:06:37 by nfurst            #+#    #+#             */
-/*   Updated: 2026/06/30 14:37:31 by nfurst           ###   ########.fr       */
+/*   Updated: 2026/06/30 14:54:14 by nfurst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_iterative_power(int nb, int power)
 {
 	int	i;
-	int res;
+	int	res;
 
 	i = 0;
 	res = 1;
